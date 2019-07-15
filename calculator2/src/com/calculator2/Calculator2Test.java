268b7dfd3075604b2463c99752eb147af871cd39
Zach <50342393+KKustos@users.noreply.github.com>
2019-07-15 14:05:51 -0500

    Add files via upload

---
+++ calculator2/src/com/calculator2/Calculator2Test.java
@@ -0,0 +1,22 @@
+package com.calculator2;
+
+public class Calculator2Test {
+
+	public static void main(String[] args) {
+		Calculator2 calc = new Calculator2();
+		calc.performOperation(10.5);
+		calc.performOperation("+");
+        calc.performOperation(5.2);
+		calc.performOperation("-");
+        calc.performOperation(2.5);
+        calc.performOperation("*");
+        calc.performOperation(2);
+		calc.performOperation("/");
+        calc.performOperation(10);
+        calc.performOperation("-");
+        calc.performOperation(4.7);
+		calc.performOperation("=");
+		calc.getResults();
+	}
+
+}
